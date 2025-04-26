d971e7293a718ccf6fd2fa195428943a553f68dd
oscar <oscar.mourasp@gmail.com>
2025-04-26 16:21:52 -0400

    realizando mejoras

--- src/config/db.ts
+++ src/config/db.ts
@@ -20,29 +20,3 @@ const sequelize = new Sequelize(
 )
 
 export default sequelize
-
-// dotenv.config({ path: './.env' })
-
-// const config = {
-//   user: process.env.DB_USER!,
-//   password: process.env.DB_PASSWORD!,
-//   server: process.env.DB_SERVER!,
-//   database: process.env.DB_NAME!,
-//   options: {
-//     encrypt: true,
-//     trustServerCertificate: true
-//   }
-// }
-
-// export async function connectDB () {
-//   try {
-//     await sql.connect(config)
-//     console.log('Conexión exitosa a SQL Server')
-//   } catch (error) {
-//     console.error('Error en la conexión a SQL Server:', error)
-//   }
-// }
-
-// connectDB()
-
-// export default sql
