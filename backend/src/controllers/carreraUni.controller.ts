7e6663b685c107cb5b330358ba2e03d4253c8861
JeremyCorrea <87151261+JeremyCorrea@users.noreply.github.com>
2025-05-08 22:00:27 -0400

    Carreras uni probadas

--- backend/src/controllers/carreraUni.controller.ts
+++ backend/src/controllers/carreraUni.controller.ts
@@ -2,18 +2,38 @@ import type { Request, Response } from 'express';
 import { carreraUni } from '../models/carreraUni.js';
 import { universidad } from '../models/universidad.js';
 
+
 // Obtener todas las carreras universitarias con su universidad
 export const listarCarrerasUniversitarias = async (req: Request, res: Response): Promise<void> => {
   try {
-    const carreras = await carreraUni.findAll({
-      include: [{
-        model: universidad,
-        attributes: ['nombreUniversidad', 'region', 'direccion'],
-      }]
-    });
+    const carreras = await carreraUni.findAll();
     res.status(200).json(carreras);
   } catch (error) {
     console.error('Error al obtener carreras universitarias:', error);
     res.status(500).json({ error: 'Error interno al obtener carreras universitarias' });
   }
+};
+
+// Obtener una carrera universitaria por ID con su universidad
+export const CarreraUniversitariaPorId = async (req: Request, res: Response): Promise<void> => {
+ return new Promise (async (resolve, reject) => {
+  try{
+    const {idCarrUni} = req.params;
+    if (!idCarrUni) {
+      res.status(400).json({ error: 'El idCarrera es obligatorio' });
+      return resolve();
+    }
+    const Carrera = await carreraUni.findByPk(idCarrUni);
+    if (Carrera){
+     res.status(200).json(Carrera);
+    } else{
+     res.status(404).json({ error: 'Carrera no encontrada' });
+    }
+    resolve();
+  }catch (error) {
+    console.error('Error al obtener carrera universitaria por ID:', error);
+    res.status(500).json({ error: 'Error interno al obtener carrera universitaria por ID' });
+    reject(error);
+  }
+  });
 };
--- backend/src/routes/carreraUniversidadRoutes.ts
+++ backend/src/routes/carreraUniversidadRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { listarCarrerasUniversitarias } from "../controllers/carreraUni.controller.js";
+import { verificarToken } from "../middleware/authMiddleware.js";
+import { CarreraUniversitariaPorId } from "../controllers/carreraUni.controller.js";
 
 const router = express.Router();
 
-router.get("/listar", listarCarrerasUniversitarias);
-
+router.get("/listar",verificarToken, listarCarrerasUniversitarias);
+router.get("/listar/:idCarrUni",verificarToken, CarreraUniversitariaPorId);
 export default router;
