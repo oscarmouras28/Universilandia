b3d84bdbf286f9b5ed016eee711e4a5b1694f74e
Wendigo <est.saez2003@gmail.com>
2025-05-09 05:34:31 -0400

    Bugs al compilar imagen docker corregido

--- frontend/src/routes/PublicOnlyRoute.tsx
+++ frontend/src/routes/PublicOnlyRoute.tsx
@@ -1,11 +1,17 @@
+// PublicOnlyRoute.tsx
 import { Navigate } from "react-router-dom";
+import { ReactNode } from "react";
 
-export default function PublicOnlyRoute({
-  children,
-}: {
-  children: JSX.Element;
-}) {
+interface Props {
+  children: ReactNode;
+}
+
+export default function PublicOnlyRoute({ children }: Props) {
   const token = localStorage.getItem("token");
 
-  return token ? <Navigate to="/" replace /> : children;
+  if (token) {
+    return <Navigate to="/" />;
+  }
+
+  return <>{children}</>;
 }
