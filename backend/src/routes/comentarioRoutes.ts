5d5f63977a1f032f3a498c5cf631caf7aae8dda0
JeremyCorrea <87151261+JeremyCorrea@users.noreply.github.com>
2025-05-14 20:53:00 -0400

    Controller comentario actualizado, todos los metodos

--- backend/src/routes/comentarioRoutes.ts
+++ backend/src/routes/comentarioRoutes.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import {actualizarComentario, crearComentario, eliminarComentario, obtenerComentariosDeBlog} from '../controllers/comentario.controller.js';
+import { verificarToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router()
 
 
-router.post('/create', crearComentario);
-router.get('/listar', obtenerComentariosDeBlog);
-router.delete('/delete/:id', eliminarComentario);
-router.get('/getComentario/:id', obtenerComentariosDeBlog); 
+router.post('/create',verificarToken, crearComentario);
+router.get('/listar/:idBlog',verificarToken, obtenerComentariosDeBlog);
+router.delete('/delete/:idComentario',verificarToken, eliminarComentario);
+router.put('/update/:idComentario',verificarToken, actualizarComentario);
 
 export default router;
